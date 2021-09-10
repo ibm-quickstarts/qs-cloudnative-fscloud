531afa80b831751c9a93ca9f33e506d6eb8584c8
Chuck Cox <chuckc@us.ibm.com>
2021-09-10 15:34:13 -0500

    test: no cluster create

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,45 +15,23 @@ provider "ibm" {
 provider "null" {
 }
 
-resource "ibm_is_vpc" "vpc1" {
-  name = "bank-vpc1"
-}
-
-resource "ibm_is_subnet" "subnet1" {
-  name                     = "bank-subnet1"
-  vpc                      = ibm_is_vpc.vpc1.id
-  zone                     = var.datacenter
-  total_ipv4_address_count = 256
-}
-
 data "ibm_resource_group" "resource_group" {
   name = var.resource_group
 }
 
-resource "ibm_resource_instance" "cos_instance" {
-  name     = "bank-cos-instance"
-  service  = "cloud-object-storage"
-  plan     = "standard"
-  location = "global"
-}
-
 resource "ibm_container_vpc_cluster" "cluster" {
-  count             = var.cluster_name == "bank_vpc_cluster1" ? 1 : 0
+  count             = var.cluster_name == "bank_vpc_cluster2" ? 1 : 0
   name              = var.cluster_name
-  vpc_id            = ibm_is_vpc.vpc1.id
+  vpc_id            = "ibm_is_vpc.vpc1.id"
   kube_version      = var.kube_version
   flavor            = var.machine_type
   worker_count      = var.default_pool_size
-  cos_instance_crn  = ibm_resource_instance.cos_instance.id
+  cos_instance_crn  = "ibm_resource_instance.cos_instance.id"
   resource_group_id = data.ibm_resource_group.resource_group.id
   zones {
-      subnet_id = ibm_is_subnet.subnet1.id
+      subnet_id = "ibm_is_subnet.subnet1.id"
       name      = var.datacenter
     }
-}
-
-data "ibm_resource_group" "group" {
-  name = var.resource_group
 }
 
 resource "null_resource" "create_kubernetes_toolchain" {
