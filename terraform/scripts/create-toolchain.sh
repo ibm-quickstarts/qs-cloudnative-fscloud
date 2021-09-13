648c908a346756030f2438e6dab9759f9e22ff96
Chuck Cox <chuckc@us.ibm.com>
2021-09-13 14:36:30 -0500

    test: kubectl cmds, instead of oc cmds

--- terraform/scripts/create-toolchain.sh
+++ terraform/scripts/create-toolchain.sh
@@ -209,14 +209,15 @@ secret=$(echo "${response}"| head -n1 | jq -j '.applications[0].secret')
 oauthserverurl=$(echo "${response}"| head -n1 | jq -j '.applications[0].oAuthServerUrl')
 appidhost=$(echo "${oauthserverurl}" | awk -F/ '{print $3}')
 
-# this may not work
-oc create secret generic bank-oidc-secret --from-literal=OIDC_JWKENDPOINTURL=$oauthserverurl/publickeys --from-literal=OIDC_ISSUERIDENTIFIER=$oauthserverurl --from-literal=OIDC_AUDIENCES=$clientid
+# trying kubectl commands in replacement of the oc commands
+echo "Testing kubectl commands, instead of oc commands here..."
+kubectl create secret generic bank-oidc-secret --from-literal=OIDC_JWKENDPOINTURL=$oauthserverurl/publickeys --from-literal=OIDC_ISSUERIDENTIFIER=$oauthserverurl --from-literal=OIDC_AUDIENCES=$clientid
 
-oc create secret generic bank-appid-secret --from-literal=APPID_TENANTID=$tenantid --from-literal=APPID_SERVICE_URL=https://$appidhost
+kubectl create secret generic bank-appid-secret --from-literal=APPID_TENANTID=$tenantid --from-literal=APPID_SERVICE_URL=https://$appidhost
 
-oc create secret generic bank-iam-secret --from-literal=IAM_APIKEY=$APPID_APIKEY --from-literal=IAM_SERVICE_URL=https://iam.cloud.ibm.com/identity/token
+kubectl create secret generic bank-iam-secret --from-literal=IAM_APIKEY=$APPID_APIKEY --from-literal=IAM_SERVICE_URL=https://iam.cloud.ibm.com/identity/token
 
-oc create secret generic mobile-simulator-secrets \
+kubectl create secret generic mobile-simulator-secrets \
   --from-literal=APP_ID_IAM_APIKEY=$APPID_APIKEY \
   --from-literal=APP_ID_MANAGEMENT_URL=$MGMTEP \
   --from-literal=APP_ID_CLIENT_ID=$clientid \
@@ -225,9 +226,9 @@ oc create secret generic mobile-simulator-secrets \
   --from-literal=PROXY_USER_MICROSERVICE=user-service:9080 \
   --from-literal=PROXY_TRANSACTION_MICROSERVICE=transaction-service:9080
 
-oc create secret generic bank-oidc-adminuser --from-literal=APP_ID_ADMIN_USER=bankadmin --from-literal=APP_ID_ADMIN_PASSWORD=password
+kubectl create secret generic bank-oidc-adminuser --from-literal=APP_ID_ADMIN_USER=bankadmin --from-literal=APP_ID_ADMIN_PASSWORD=password
 
-oc create secret generic bank-db-secret --from-literal=DB_SERVERNAME=creditdb --from-literal=DB_PORTNUMBER=5432 --from-literal=DB_DATABASENAME=example --from-literal=DB_USER=postgres --from-literal=DB_PASSWORD=postgres
+kubectl create secret generic bank-db-secret --from-literal=DB_SERVERNAME=creditdb --from-literal=DB_PORTNUMBER=5432 --from-literal=DB_DATABASENAME=example --from-literal=DB_USER=postgres --from-literal=DB_PASSWORD=postgres
 
 # create the toolchain
 PARAMETERS="region_id=$TOOLCHAIN_REGION&resourceGroupId=$RESOURCE_GROUP_ID&autocreate=true"`
